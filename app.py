616358cb276fd6b4b0c78e57ea9c26590ed06ed4
angelapav <pav0016@copperfieldcollege.vic.edu.au>
2025-06-25 12:55:28 +1000

    update layout

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
-import customtkinter
+import  customtkinter as ctk
 
-app = customtkinter.CTk()
+app = ctk.CTk()
 app.title("SugarSchedule")
-app.geometry("1000x600")
+app.geometry("900x500")
 
-
+#app window grid
 app.grid_columnconfigure(0, weight=1)
-app.grid_columnconfigure(0, weight=4)
+app.grid_columnconfigure(1, weight=1)
 app.grid_rowconfigure(0, weight=1)
+app.grid_rowconfigure(1, weight=1)
+
+#top frame
+topFrame= ctk.CTkFrame(app, border_width=2)
+topFrame.grid(row=0, column=0, columnspan=2, sticky="nsew", padx=5, pady=5)
+topFrame.grid_columnconfigure(0, weight=1)
+topFrame.grid_columnconfigure(1, weight=1)
+titleLabel = ctk.CTkLabel(topFrame, text= "SugarSchedule", font= ("Helvetica", 40))
+titleLabel.grid(row=0, column=0, sticky="nsew")
+
+
+
 
 
 
