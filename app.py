3c7937436ee2f47707ba84fbc931b60fe63d06f4
angelapav <pav0016@copperfieldcollege.vic.edu.au>
2025-06-30 12:26:22 +1000

    update layouts

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import  customtkinter as ctk
 
 app = ctk.CTk()
 app.title("SugarSchedule")
-app.geometry("900x500")
+app.geometry("800x500")
 
 #app window grid
 app.grid_columnconfigure(0, weight=1)
@@ -11,14 +11,35 @@ app.grid_rowconfigure(0, weight=1)
 app.grid_rowconfigure(1, weight=1)
 
 #top frame
-topFrame= ctk.CTkFrame(app, border_width=2)
-topFrame.grid(row=0, column=0, columnspan=2, sticky="nsew", padx=5, pady=5)
+topFrame= ctk.CTkFrame(app, border_width=1)
+topFrame.grid(row=0, column=0, columnspan=2, sticky="nsew", padx=2, pady=2)
 topFrame.grid_columnconfigure(0, weight=1)
 topFrame.grid_columnconfigure(1, weight=1)
-titleLabel = ctk.CTkLabel(topFrame, text= "SugarSchedule", font= ("Helvetica", 40))
+titleLabel = ctk.CTkLabel(topFrame, text= "SugarSchedule", font= ("Helvetica", 30))
 titleLabel.grid(row=0, column=0, sticky="nsew")
+homeButtonFrame = ctk.CTkFrame(topFrame)
+homeButtonFrame.grid(row=0, column=1, sticky="e", padx=5, pady=5)
+
+#left frame
+leftFrame= ctk.CTkFrame(app, border_width=4)
+leftFrame.grid(row=1, column=0, sticky="nsew", padx=2, pady=2)
+leftFrame.grid_rowconfigure(0, weight=1)
+leftFrame.grid_rowconfigure(1, weight=1)
+leftFrame.grid_rowconfigure(2, weight=1)
+leftFrame.grid_columnconfigure(0, weight=1)
+leftFrame.grid_columnconfigure(1, weight=1)
 
 
+
+
+#right frame
+rightFrame= ctk.CTk(app, border_width=4)
+rightFrame.grid(row=1, column=2, sticky="nsew", padx=2, pady=2)
+rightFrame.grid_rowconfigure(0, weight=1)
+rightFrame.grid_rowconfigure(1, weight=1)
+rightFrame.grid_rowconfigure(2, weight=1)
+rightFrame.grid_columnconfigure(0, weight=1)
+rightFrame.grid_columnconfigure(1, weight=1)
 
 
 
